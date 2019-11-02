0348f71e266e24927e39c7ce7ab56cb893919527
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-11-02 13:41:07 -0700

    starter files with server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+}
+
+app.use(routes);
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'localhost:3000/googlebooks',
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true
+    }
+);
+
+app.listen(PORT, () => console.log(`API Server listening on PORT ${PORT}`));
+
