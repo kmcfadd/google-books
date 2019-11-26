bb373e784450ffb73c85d6c6685ac4c4fc729c61
Kyle McFadden <burntwist@sbcglobal.net>
2019-11-25 16:23:28 -0800

    fix connection

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 mongoose.connect(
-  process.env.MONGODB_URI || "localhost:3000/googlebooks",
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
   {
     useCreateIndex: true,
     useNewUrlParser: true
